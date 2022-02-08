de39fcf5b679b2fbc9401f802d937584271347c1
Eracler Dio <ilasaz1988@gmail.com>
2022-02-08 22:57:17 +0300

    lesson 1

---
+++ 7kyu.js
@@ -0,0 +1,23 @@
+/*Return the number (count) of vowels in the given string.
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+The input string will only consist of lower case letters and/or spaces.*/
+
+/*function getCount(str) {
+  var vowelsCount = 0;
+ return str.split("").filter(item => (item == 'a' || item ==  'e' || item ==  'i' || item ==  'o' || item ==  'u' )).length 
+}
+
+console.log(getCount("hgreaegj gyityf hbuyit"))*/
+
+
+
+/*Добро пожаловать. В этой ката вас просят возвести в квадрат каждую цифру числа и соединить их.
+Например, если мы пропустим через функцию 9119, получится 811181, потому что 9 2 равно 81, а 1 2 равно 1.
+Примечание . Функция принимает целое число и возвращает целое число.*/
+
+/*function squareDigits(num){
+ return Number(String(num).split("").map(i => (i*i)).join(""));
+
+}
+
+console.log(squareDigits(2456))*/
